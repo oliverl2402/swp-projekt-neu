60acd5fde5b4e6eff39179493d9903b12045ccc3
Smeit <smeitlp@gmail.com>
2024-01-31 10:38:47 +0100

    hallo oli

---
+++ src/main/java/com/swp/skiticket/SkiticketApplication.java
@@ -0,0 +1,13 @@
+package com.swp.skiticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkiticketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkiticketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swp/skiticket/SkiticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp.skiticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkiticketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
