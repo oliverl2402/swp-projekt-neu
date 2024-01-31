efb6c7ef20e20c2dede3098da4be4796fe079d2d
oliverl2402 <oliver.liedl.student@htl-hallein.at>
2024-01-31 10:10:13 +0100

    1

---
+++ src/main/java/com/example/swpprojektneu/SwpProjektNeuApplication.java
@@ -0,0 +1,13 @@
+package com.example.swpprojektneu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwpProjektNeuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwpProjektNeuApplication.class, args);
+    }
+
+}
